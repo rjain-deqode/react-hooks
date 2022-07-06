9c771dfc65f2388cfe2582c896633132341c66e5
jraksha <rjain@deqode.com>
2022-07-06 18:42:36 +0530

    complex counter

--- src/components/UseStateCounter.js
+++ src/components/UseStateCounter.js
@@ -6,19 +6,49 @@ export const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+
+  const increaseComplex = () => {
+      setTimeout(()=>{
+        //   setValue(value + 1)
+        setValue((prevState)=>{
+           return prevState + 1
+        });
+      }, 2000)
+  };
   return (
-    <div>
-      <h1>useState simple counter </h1>
-      <h3>{value}</h3>
-      <button style={{margin:'0 5px 0 0'}} className="btn" onClick={() => setValue(value + 1)}>
-        Increase
-      </button>
-      <button style={{margin:'0 5px 0 0'}} className="btn" onClick={() => setValue(value - 1)}>
-        Decrease
-      </button>
-      <button className="btn" onClick={reset}>
-        Reset
-      </button> 
-    </div>
+    <>
+      <div>
+        <h1>useState simple counter </h1>
+        <h3>{value}</h3>
+        <button
+          style={{ margin: "0 5px 0 0" }}
+          className="btn"
+          onClick={() => setValue(value + 1)}
+        >
+          Increase
+        </button>
+        <button
+          style={{ margin: "0 5px 0 0" }}
+          className="btn"
+          onClick={() => setValue(value - 1)}
+        >
+          Decrease
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </div>
+      <div>
+        <h1>useState complex counter </h1>
+        <h3>{value}</h3>
+        <button
+          style={{ margin: "0 5px 0 0" }}
+          className="btn"
+          onClick={increaseComplex}
+        >
+          Increase
+        </button>
+      </div>
+    </>
   );
 };
