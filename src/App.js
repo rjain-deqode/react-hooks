fea3343e32f536afb167ff4231e20263eba97654
jraksha <rjain@deqode.com>
2022-07-06 13:27:48 +0530

    useState basic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { UseStateBasics } from "./components/UseStateBasics";
+
+function App() {
+  return (
+    <div className="container">
+     <UseStateBasics />
+    </div>
+  );
+}
+
+export default App;
